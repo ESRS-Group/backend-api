594378d985537da24f67552e7d1e8727bff2c785
swoody2527 <104504405+swoody2527@users.noreply.github.com>
2025-03-18 18:31:22 +0000

    forgot to save stuff, whoops

--- controllers.py
+++ controllers.py
@@ -4,6 +4,21 @@ import models
 
 app = Flask(__name__)
 
+"""
+TO DO
+
+Need to think about proper error handling, how this might work in the front end.
+Need to start unit testing.
+Hosting will need to be done if 
+
+"""
+
+
+"""
+Example query
+/api/articles?genre=World&source=BBC-News
+
+"""
 @app.route("/api/articles", methods=["GET"])
 def get_articles():
     genre = request.args.get("genre")
@@ -12,6 +27,10 @@ def get_articles():
 
     return articles, 200
 
+"""
+Example query
+/api/articles/67bf73248d2ae870c932d262
+"""
 
 @app.route("/api/articles/<string:id>")
 def get_article_by_id(id):
@@ -21,6 +40,6 @@ def get_article_by_id(id):
     return jsonify({"error": "Article not found"}), 404
 
 
-
 if __name__ == "__main__":
     app.run(debug=True)
+
